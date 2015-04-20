6d28602c7338f49a0edb3ab8779f59aafe2daaed
zhouqing <zhouqing3737@gmail.com>
2015-04-19 22:05:07 -0400

    debug

--- network/peerdocsnode.go
+++ network/peerdocsnode.go
@@ -446,7 +446,7 @@ func fetchDocHttp(w http.ResponseWriter, req *http.Request){
     responseB, _ := json.Marshal(p)
     responsestring = string(responseB)
     //responsestring = "Access-Control-Allow-Credentials:true\nAccess-Control-Allow-Headers:Origin,x-requested-with\nAccess-Control-Allow-Methods:PUT,PATCH,GET,POST\nAccess-Control-Allow-Origin:*\nAccess-Control-Expose-Headers:Content-Length" + responsestring
-    responsestring="{\"docs\":"+responsestring+"}"
+    responsestring="{\"doc\":"+responsestring+"}"
     io.WriteString(w,responsestring)
 }
 
