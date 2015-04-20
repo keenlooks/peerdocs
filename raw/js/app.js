bae6afd4049704ba4225f75d5b3ca48c6bc425b1
zhouqing <zhouqing3737@gmail.com>
2015-04-20 18:31:53 -0400

    update frontend

--- raw/js/app.js
+++ raw/js/app.js
@@ -1,10 +1,56 @@
-window.PD = Ember.Application.create();
+window.PD = Ember.Application.create({});
 
 //PD.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+DS.RESTSerializer.reopen({
+    serializeBelongsTo: function(record, json, relationship) {
+        var key = relationship.key,
+            belongsTo = Ember.get(record, key);
+        key = this.keyForRelationship ? this.keyForRelationship(key, "belongsTo") : key;
+        
+        if (relationship.options.embedded === 'always') {
+            json[key] = belongsTo.serialize();
+        }
+        else {
+            return this._super(record, json, relationship);
+        }
+    },
+    serializeHasMany: function(record, json, relationship) {
+        var key = relationship.key,
+            hasMany = Ember.get(record, key),
+            relationshipType = DS.RelationshipChange.determineRelationshipType(record.constructor, relationship);
+        
+        if (relationship.options.embedded === 'always') {
+            if (hasMany && relationshipType === 'manyToNone' || relationshipType === 'manyToMany' ||
+                relationshipType === 'manyToOne') {
+                
+                json[key] = [];
+                hasMany.forEach(function(item, index){
+                    json[key].push(item.serialize());
+                });
+            }
+        
+        }
+        else {
+            return this._super(record, json, relationship);
+        }
+    }
+});
+
+
+
+
+
 PD.ApplicationAdapter = DS.RESTAdapter.extend({
   namespace: 'api',
   host: 'http://localhost:8080',
 });
+
+/*PD.ApplicationAdapter.map('PD.Docdelt', {
+  doccgs: { embedded: 'always' }
+});
+*/
+
 
 /*PD.ApplicationAdapter = DS.RESTAdapter.extend({
   namespace: 'api',
--- raw/js/router.js
+++ raw/js/router.js
@@ -35,37 +35,79 @@ setupController: function(controller,doc){
         console.log('selection-change', range)
       });
 		*/
-    var docdelt = this.store.createRecord('docdelt');
-    //console.log(docdelta);
-    docdelt.save();
 
+    //,'doccgs':[{'location':33,'mod':'ssss'},{'location':33,'mod':'dddd'}]}
+    var docdelt = this.get("store").createRecord('docdelt',{"docid":doc.get("id")});
+    console.log(docdelt.serialize());
+    docdelt.save();
+    var freeze=false;
       var that=this;
+      var cursorpos=0;
       editor.on('text-change', function(delta, source) {
+
+        console.log("CHANGE FIRES "+freeze);
       	var range = editor.getSelection();
 
       	$.each(delta.ops,function(i,el){
-			   if(el.insert!=null && range!=null){
-   				console.log(el.insert+" @ "+(range.start-el.insert.length));
-          var cg=that.store.createRecord('doccg',{'location':(range.start-el.insert.length),'mod':el.insert});
-          docdelt.get("doccgs").addObject(cg);
-          //console.log(docdelta);
+          console.log(el+':'+range);
+			   if(!freeze&&el.insert!=null && range!=null){
+          cursorpos=range.start;
+   				console.log("insert :"+el.insert+" @ "+(range.start-el.insert.length));
+          var cg=that.get("store").createRecord('doccg',{'location':(range.start-el.insert.length),'mod':el.insert});
+          docdelt.get("doccgs").pushObject(cg);
+//docdelt.send('becomeDirty');
+         //console.log(docdelt.serialize());
 			   }
-			   else if(el.insert!=null){
-				  console.log(el.insert+" @ 0");
-          var cg0=that.store.createRecord('doccg',{'location':0,'mod':el.insert});
-          docdelt.get("doccgs").addObject(cg0);
-          //console.log(docdelta);
+         else if(!freeze&&el.delete!=null && range!=null){
+           console.log("delete: "+el.delete+" @ "+(range.start));
+           var deletestr="";
+           for(var i=el.delete;i>0;i--){
+              deletestr=deletestr+"\\b";
+              //console.log(deletestr);
+           }
+
+          var cg2=that.get("store").createRecord('doccg',{'location':range.start,'mod':deletestr});
+         console.log(deletestr);
+         deletestr="";
+          docdelt.get("doccgs").pushObject(cg2);
+         }
+			   else if(!freeze&&el.insert!=null){
+				  console.log("insert :"+el.insert+" @ 0");
+          var cg0=that.get("store").createRecord('doccg',{'location':0,'mod':el.insert});
+          docdelt.get("doccgs").pushObject(cg0);
+          //docdelt.send('becomeDirty');
+           //console.log(docdelt.serialize());
 			   }
 		  });
         
     });
 
        refresher=setInterval(function(){
+
+       // console.log(docdelt.serialize());
+ 
         //console.log("hi");
-        docdelt.get('store').commit().then(function(docdelt){
+        //docdelt.set("docid",doc.get("id"));
+        docdelt.save().then(function(docdelt){
+          docdelt.get("doccgs").clear();
+          //console.log(docdelt.serialize());
+          doc.reload().then(function(doc){
+
+            freeze=true;
+            editor.setText(doc.get('ctext'));
+            freeze=false;
+
+            console.log(doc.get('ctext'));
+            editor.setSelection(cursorpos,cursorpos);
+          });
+          
+
+        });
+
+        /*.then(function(docdelt){
           docdelt.clear();
           doc.reload();
-        });
+        });*/
 
         },5000);
 
