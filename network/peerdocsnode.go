eb3043b752fa710c5a6b9180a4224dd817011456
Keane Lucas <keenlooks@gmail.com>
2015-04-21 03:23:04 -0400

    attempt fix cursor / clean up dead code

--- network/peerdocsnode.go
+++ network/peerdocsnode.go
@@ -140,7 +140,7 @@ func listDocs()([]Docmeta){
         fopened, err := os.Open(docFolderPath+f.Name())
         if(f.Name()[0]=='.'){continue}
         if(err != nil){
-            fmt.Println("cannot open doc")
+            fmt.Println("cannot open "+f.Name()+" in list")
         }
         buf := make([]byte, 128)
         count, _ := fopened.Read(buf)
@@ -292,22 +292,6 @@ func updateDocNodeWithId(host Host){
 
 }
 
-/*func joinGroup(argument string)(bool){
-	//will connect to token ring of group described by base64 encoded "argument"
-    
-    //create doc with contents 
-    f, err := os.Create(docFolderPath+strings.Split(strings.Split(argument, "<DocID>")[1], "</DocID>")[0])
-    if err != nil{
-        return false
-    }
-
-    f.WriteString(argument)
-
-    f.Close()
-
-	return true
-}*/
-
 func leaveGroup(argument string)(bool){
 
     //TODO tell all other nodes in group contained in "argument" to delete your IP address by adding something to the token
@@ -331,37 +315,6 @@ func sendResponse(conn net.Conn, response FrontEndResponse){
     encoder.Encode(p)
     conn.Close()
 }
-
-/*func handleConn(conn net.Conn){
-
-// hello world, the web server
-    
-	//receive command and argument - command can be UPDATE, FETCH, LIST, JOIN, LEAVE, or CREATE
-    command, argument, changearray := receiveCommand(conn)
-
-    //process command and argument
-    response := process(command, argument, changearray)
-
-    //send response back to client
-    sendResponse(conn, response)
-}*/
-
-/*func listenToken(){
-    ln, err := net.Listen("tcp", tokenListenPort)
-
-    if err != nil {
-        fmt.Println("error listening for connection")
-    }
-
-    for {
-        conn, err := ln.Accept() // Try to accept a connection
-        if err != nil {
-            fmt.Println("error accepting connection")
-        }
-
-        go handleToken(conn)
-    }
-}*/
 
 func updateFile(DocID string)(bool){
 
@@ -432,6 +385,9 @@ func handleToken(token Token)(Token){
             if change.Position <= localchange.Position {
                 localchange.Position += len(change.Charstoappend)//-(strings.Count(change.Charstoappend,backspacestring)*(len(backspacestring)+1)))
             }
+            if change.Position <= cursorPos[token.DocID] {
+                cursorPos[token.DocID] += (len(change.Charstoappend)-strings.Count(change.Charstoappend,backspacestring)*(len(backspacestring)+1))
+            }
         }
     }
     token.Changes = append(token.Changes[numPastLocalChanges[token.DocID]:], localChanges[token.DocID]...)
@@ -458,20 +414,6 @@ func handleToken(token Token)(Token){
 
     fmt.Println("could not update "+token.DocID)
     return token
-
-    /*return // will be removed out once rest is implemented
-    
-    //TODO - find next available node by looking through group list and trying to connect to each one in order (or some other way)
-        //TODO - try first host after you
-    conn2, err := net.Dial("tcp", "<host>"+tokenListenPort)
-    for err != nil {
-        //TODO - if doesnt work, increment to next possible host and try in for loop
-        conn2, err = net.Dial("tcp", "<host>"+tokenListenPort)
-    }
-
-    encoder := gob.NewEncoder(conn2)
-    encoder.Encode(token)
-    conn2.Close()*/
 }
 
 
@@ -484,7 +426,7 @@ func updateChangesHttpGet(w http.ResponseWriter, req *http.Request){
     req.Body.Read(buf)
     decoder := json.NewDecoder(req.Body)
     decoder.Decode(dd)
-    fmt.Println(dd)
+    //fmt.Println(dd)
     DocID := strconv.Itoa(dd.Id) //strings.Split(req.URL.Path, "docdelts/")[1]
     w.Header().Set("Access-Control-Allow-Credentials", "true")
     w.Header().Set("Access-Control-Allow-Headers","Origin,x-requested-with,Content-Type")
@@ -492,7 +434,7 @@ func updateChangesHttpGet(w http.ResponseWriter, req *http.Request){
     w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1")
     w.Header().Set("Access-Control-Expose-Headers", "Content-Length,Content-Type")
     if req.Method == "PUT"{
-        fmt.Println("updating "+DocID)
+        //fmt.Println("updating "+DocID)
         cursorPos[DocID] = dd.Cursorpos
         if !updateChanges(DocID, dd.Doccgs){ fmt.Println("update changes for "+DocID+" didnt work")}
 
@@ -510,12 +452,9 @@ func updateChangesHttpGet(w http.ResponseWriter, req *http.Request){
         }*/    
         //THIS PART NEEDS TO BE REMOVED AFTER INTEGRATION WITH TOKEN PASSING
 
-       // dd.Id,_ = strconv.Atoi(DocID)
         p := &dd
-        //encoder.Encode(p)
         responseB, _ := json.Marshal(p)
         responsestring := string(responseB)
-        //responsestring = "Access-Control-Allow-Credentials:true\nAccess-Control-Allow-Headers:Origin,x-requested-with\nAccess-Control-Allow-Methods:PUT,PATCH,GET,POST\nAccess-Control-Allow-Origin:*\nAccess-Control-Expose-Headers:Content-Length" + responsestring
         
         if docmodified[DocID] {
             responsestring = "{\"docdelt\": {\"id\":11223344,\"docid\":"+DocID+",\"cursor\":"+strconv.Itoa(cursorPos[DocID])+",\"doccgs\":[{\"id\":0,\"location\":0, \"mod\":\"a\"}]}}"
@@ -612,82 +551,6 @@ func fetchDoc(DocID string)(Docfetch){
 
 
 
-/*func process(command string, argument string, changearray []Change)(FrontEndResponse){   
-
-    response := ""
-    var changes = []Change{}
-    
-    switch command {
-    	case "UPDATE":
-    		//requires DocID as argument
-    		//used to push updates that user is providing to document. append changes to change array in token
-    		if argument == "" || len(changearray) == 0 {
-    			response = "command requires DocID as argument and changearray to have changes"
-    			break
-    		}
-
-            //append changes to global localChanges slice
-            updateChanges(argument, changearray)
-
-    	case "FETCH":
-    		//requires DocID as argument
-    		//used by UI to ask for official changes to document (made official when token has passed through node and other users' changes have been added)
-    		if argument == "" {
-    			//TODO send error message back to requester
-    			response = "command requires argument"
-    			break
-    		}
-
-            //return official changes contained in officialChanges array
-            changes = fetchOfficialChanges(argument)
-
-    		
-    	case "LIST":
-    		//used by UI to see what Docs this node has access to (has access keys for)
-   			response = listDocs()
-
-    	case "JOIN":
-    		//requires DocID as argument
-    		//used by UI with argument of base64 encoded string of DocID:groupList:GroupKey
-    			//DocID is a string in form <creating host MAC>.<number identifier>
-    			//groupList is array of IPs in string form to check for bootstrapping
-    		if argument == ""{
-    			response = "command requires argument"
-    			break
-    		}
-
-    		if joinGroup(argument){
-    			response = "success"
-    		}else{
-    			response = "fail"
-    		}
-
-    	case "LEAVE":
-    		//requires DocID as argument
-    		//used by UI to delete a key (the doc file) and remove itself from a group sends an update to all nodes to remove it from the list.
-    		if argument == ""{
-    			response = "command requires argument"
-    			break
-    		}
-            if leaveGroup(argument){
-                response = "success"
-            }else{
-                response = "fail"
-            }
-
-    	case "CREATE":
-    		//used by UI to initiate a Doc, returns a DocID
-            response = createDoc()
-
-    }
-
-
-    return FrontEndResponse{
-    	Responsestring: response,
-    	Changearray: changes,
-    }
-}*/
-
 func joinGroupsFromDoc(){
     doclist := listDocs()
     for _, doc := range doclist {
@@ -711,50 +574,7 @@ func joinGroupsFromDoc(){
 }
 
 func main() {
-    //testing json request
-    /*conn, err := net.Dial("tcp", "localhost:8080")
-    encoder := json.NewEncoder(conn)
-    p := &FrontEndRequest {
-         Command: "LIST", 
-         Argument: "ertqewqr",
-         Changearray: []string{"hello"}}
-    fmt.Println(p)
-    bla, _ := json.Marshal(p)
-    fmt.Println(bla)
-    encoder.Encode(p)
-    conn.Close()
-    fmt.Println("done");
-    return*/
-
-    
-    //requests sent to the server must be in form: 
-    //  {"command":"<command>","argument":"<argument>","changearray":<position:string array of changes>}
-    
-
-    /*ln, err := net.Listen("tcp", listenPort)
-
-    if err != nil {
-        fmt.Println("error listening for connection")
-    }*/
-    
-    //start listening for tokens
-    //go listenToken()
-/*
-    host1 := Host{}
-    host2 := Host{}
-    host1.Name = "NodeA"
-    host2.Name = "NodeB"
-    host1.Address = ""
-    host2.Address = "128.237.198.207"
-    host1.DocID = "1"
-    host2.DocID = "1"
-    host1.DocKey = "1"
-    host2.DocKey = "1"
-    hostarray := Hostarray{}
-    hostarray.Hosts = []Host{host1,host2}
-    responseB, _ := json.Marshal(&hostarray)
-    fmt.Println(string(responseB))
-    return*/
+   
 
     fmt.Println("Server running...")
     selfName = os.Args[1]
@@ -772,49 +592,4 @@ func main() {
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
-
-    /*
-    for {
-        conn, err := ln.Accept() // Try to accept a connection
-        if err != nil {
-            fmt.Println("error accepting connection")
-        }
-
-        go handleConn(conn)
-    }
-    */
 }
-/*
-http://localhost:8080/api/docdelts
-
-{
- 'docdelts':
-{
-  id:43
-  doccgs:[
-  {
-    id: 66
-    location: 43
-    mod:'insertted text'
-  },
-   {
-    id: 67
-    location: 43
-    mod:'insertted text'
-  },
-  {
-    id: 68
-    location: 43
-    mod:'insertted text'
-  },
-
-  ]
-}
-
-}
-*/
-
-// GET http://localhost:8080/api/docs/ID
-
-
-
