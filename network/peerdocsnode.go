7dfd0967aeaf22d8842311cb2f53a061c249137e
Keane Lucas <keenlooks@gmail.com>
2015-04-21 09:57:20 -0400

    invitation accept sends nil as name

--- network/peerdocsnode.go
+++ network/peerdocsnode.go
@@ -207,7 +207,7 @@ func inviteNodeHttp(w http.ResponseWriter, req *http.Request){
     }
     if(hostinvite.TypeRequest == "join"){
         fmt.Println("joining "+ hostinvite.Name)
-        joinGroup(hostinvite.Name, hostinvite.Address, strconv.Itoa(hostinvite.DocID), hostinvite.DocKey, false)
+        joinGroup(nil, hostinvite.Address, strconv.Itoa(hostinvite.DocID), hostinvite.DocKey, false)
     }
 
     responseB, _ := json.Marshal(hostinvite)
